71b0e9aebada991a72d4ca8e6d4846542f24bc4f
nujabec <nujabec@gmail.com>
2023-12-17 17:51:26 +0900

    1st commit

---
+++ src/components/Results.tsx
@@ -0,0 +1,27 @@
+type ResultsPropType = {
+    results: {
+        country: string;
+        cityName: string;
+        temperature: string;
+        conditionText: string;
+        icon: string;
+    }
+}
+
+const Results = (props: ResultsPropType) => {
+    return (
+        <div>
+            {props.results.country && <div className="results-country">{props.results.country}</div>}
+            {props.results.cityName && <div className="results-city">{props.results.cityName}</div>}
+            {props.results.temperature && <div className="results-temp">{props.results.temperature} <span>℃</span></div>}
+            {props.results.conditionText &&
+                <div className="results-condition">
+                    <img src={props.results.icon} alt="icon" />
+                    <p>{props.results.conditionText}</p>
+                </div>
+            }
+        </div>
+    );
+};
+
+export default Results;
