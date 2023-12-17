b2fcb525d52aea53b38e71bb15dfddeecf2fa76b
nujabec <nujabec@gmail.com>
2023-12-17 23:50:47 +0900

    change temp span design

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -14,7 +14,7 @@ const Results = ({results}: ResultsPropType) => {
         <>
             {country && <div className="results-country">{country}</div>}
             {cityName && <div className="results-city">{cityName}</div>}
-            {temperature && <div className="results-temp">{temperature} <span>℃</span></div>}
+            {temperature && <div className="results-temp">{temperature}<span>℃</span></div>}
             {conditionText &&
                 <div className="results-condition">
                     <img src={icon} alt="icon" />
